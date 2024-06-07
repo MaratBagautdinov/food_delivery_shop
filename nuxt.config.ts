e0f368c2a92e8ad55cc5c31218999f61d1caef5e
Marat <marat.bagautdinov522@mail.ru>
2024-06-07 18:17:44 +0300

    useFileStorage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: [ 'nuxt-swiper'],
+  modules: ['nuxt-file-storage', 'nuxt-swiper'],
   ssr: false
 })
